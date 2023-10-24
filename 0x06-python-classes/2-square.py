383cdb6731686daae699df8f22bd000cb8a571a5
HleyHub <hashleyed@gmail.com>
2023-10-24 13:58:58 -0700

    A class Square that defines a square by: (based on 1-square.py)

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for Square class"""
+
+
+class Square:
+    """Attributes for Square class
+       size(int): size of square
+    """
+
+    def __init__(self, size=0):
+        """size intitialized if not exception"""
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >=0")
+        self.__size = size
