7142d6ffff12ac90cf28ea196b1477e44a24010b
HleyHub <hashleyed@gmail.com>
2023-11-01 09:37:33 -0700

    A function that prints a square with the character #

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+A function that prints prints a square
+"""
+
+def print_square(size):
+    """
+    Return: A square with the character #
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for j in range(size):
+         print("#" * size)
